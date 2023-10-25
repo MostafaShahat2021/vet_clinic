e60459904785835d31a791731127d3999cab0cce
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-10-25 20:37:08 +0300

    Update tabel and transactions

--- queries.sql
+++ queries.sql
@@ -21,3 +21,45 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- transaction 
+-- update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+BEGIN;
+
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+
+-- delete all records in the animals table, then roll back the transaction.
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+-- Inside a transaction:
+
+    -- Delete all animals born after Jan 1st, 2022.
+    -- Create a savepoint for the transaction.
+    -- Update all animals' weight to be their weight multiplied by -1.
+    -- Rollback to the savepoint
+    -- Update all animals' weights that are negative to be their weight multiplied by -1.
+    -- Commit transaction
+    BEGIN;
+    DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+    SAVEPOINT delete_by_birth_of_date;
+    UPDATE animals SET weight_kg = weight_kg * -1;
+    ROLLBACK TO delete_by_birth_of_date;
+    UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+    COMMIT;
